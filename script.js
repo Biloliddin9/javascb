4f399a89cbb615fd3801f7e1fe4316e253d8e1c9
Biloliddin9 <biloliddinumarov9@gmail.com>
2025-06-25 18:31:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let a = prompt('Ism Famila')
+let b = prompt('Yosh')
+let d = prompt('Hobby')
+let c = prompt('Yashash Joyi')
+let n = prompt ('Yoqtirgan fanini')
+
+
+console.log(a);
+console.log(b);
+console.log(d);
+console.log(c);
+console.log(n);
+
+
+
